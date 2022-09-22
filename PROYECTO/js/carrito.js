4bc6440061d81f4563ea2692a399c80d1de685b5
Maxibolche14 <maxibolche@gmail.com>
2022-09-22 19:43:29 -0300

    js9

--- PROYECTO/js/carrito.js
+++ PROYECTO/js/carrito.js
@@ -1,10 +1,17 @@
 var listaCarrito = []
 let listadoCarrito = document.getElementById("listadoCarrito");
+let total = document.getElementById("total");
+let Subtotal = document.getElementById("Subtotal");
+
+
 function cargoProductos() {
 
     listaCarrito = Storage.getLocalStorage('carrito');
     console.log(listaCarrito);
-    generar_card(listaCarrito)
+    generar_card(listaCarrito);
+    calculoTotal(listaCarrito);
+    console.log(total);
+
 }
 
 window.onload = cargoProductos();
@@ -14,7 +21,7 @@ function generar_card(listaCarrito) {
 
     listaCarrito.forEach(product => {
         let new_div = document.createElement("div");
-        new_div.className = "card_producto col-xs-12 col-sm-12 col-md-2 col-lg-2";
+        new_div.className = "card_producto col-xs-12 col-sm-12 col-md-2 col-lg-3";
 
         let div_container = document.createElement("div");
         let img_producto = document.createElement("img");
@@ -37,9 +44,35 @@ function generar_card(listaCarrito) {
         div_container.appendChild(new_Marca);
         div_container.appendChild(new_Precio);
         new_div.appendChild(div_container);
-
         listadoCarrito.appendChild(new_div);
+
     });
-    
 
 }
+
+function calculoTotal(listaCarrito) {
+    let valortotal = 0;
+
+    listaCarrito.forEach(product => {
+
+        valortotal += parseInt(product.PrecioProducto);
+
+    });
+
+    let div_Total = document.createElement('div');
+    div_Total.textContent = "$ " + valortotal + " Impuestos Inc.";
+    total.appendChild(div_Total);
+
+    calculoSubTotal(valortotal);
+}
+
+
+function calculoSubTotal(valortotal) {
+    let Subtotal = 0;
+
+    Subtotal = parseInt(valortotal) / 0.22;
+
+}
+
+
+
